5715198d3ac645c953f956898bc1b7491393b9c5
SwarrenB <cauti1@yandex.ru>
2024-10-11 14:49:49 +0400

    sprint 1 increment 1 fix url generatin process

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -14,6 +14,9 @@ const letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
 func GenerateURL(n int) string {
 	b := "/"
+	if n == 0 {
+		n = 5
+	}
 	for i := 0; i < n; i++ {
 		b += string(letters[rand.IntN(len(letters))])
 	}
