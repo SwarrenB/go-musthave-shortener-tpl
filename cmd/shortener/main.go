d03040d1eea53e2543a178e022c450ab6bdbd790
SwarrenB <cauti1@yandex.ru>
2024-10-23 21:55:44 +0400

    iter6: add request details logging

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -2,10 +2,11 @@ package main
 
 import (
 	"log"
-	"os"
 
 	"github.com/SwarrenB/go-musthave-shortener-tpl/internal/app/config"
 	"github.com/SwarrenB/go-musthave-shortener-tpl/internal/app/handlers"
+	"github.com/SwarrenB/go-musthave-shortener-tpl/internal/app/logger"
+	"github.com/SwarrenB/go-musthave-shortener-tpl/internal/app/middleware"
 	"github.com/SwarrenB/go-musthave-shortener-tpl/internal/app/service"
 	"github.com/gin-gonic/gin"
 )
@@ -13,15 +14,16 @@ import (
 func main() {
 	if err := run(); err != nil {
 		log.Fatal(err)
-		os.Exit(1)
 	}
 }
 
 func run() error {
+	logger.Initialize("Info")
 	appConfig := config.CreateGeneralConfig()
 	service := service.CreateShortenerService()
 	router := gin.Default()
 	handler := handlers.CreateGinHandler(service, *appConfig)
+	router.Use(middleware.WithLogging)
 	router.GET("/:id", handler.GinGetRequestHandler())
 	router.POST("/", handler.GinPostRequestHandler())
 	router.Run(appConfig.ServerAddress)
---
+++ internal/app/middleware/middleware.go
@@ -0,0 +1,50 @@
+package middleware
+
+import (
+	"time"
+
+	"github.com/SwarrenB/go-musthave-shortener-tpl/internal/app/logger"
+	"github.com/gin-gonic/gin"
+	"go.uber.org/zap"
+)
+
+// в виду удобства были использованы встроенные инструменты из Gin,
+// но возможность создания кастомного Writer была также рассмотрена
+// type (
+// 	responseData struct {
+// 		status int
+// 		size   int
+// 	}
+
+// 	loggingResponseWriter struct {
+// 		http.ResponseWriter
+// 		responseData *responseData
+// 	}
+// )
+
+// func (r *loggingResponseWriter) Write(b []byte) (int, error) {
+// 	size, err := r.ResponseWriter.Write(b)
+// 	r.responseData.size += size
+// 	return size, err
+// }
+
+// func (r *loggingResponseWriter) WriteHeader(statusCode int) {
+// 	r.ResponseWriter.WriteHeader(statusCode)
+// 	r.responseData.status = statusCode
+// }
+
+func WithLogging(c *gin.Context) {
+	start := time.Now()
+
+	c.Next()
+	duration := time.Since(start)
+
+	logger.Log.Info(
+		"request details",
+		zap.String("uri", c.Request.URL.Path),
+		zap.String("method", c.Request.Method),
+		zap.Int("status", c.Writer.Status()),
+		zap.Duration("duration", duration),
+		zap.Int("size", c.Writer.Size()),
+	)
+}
